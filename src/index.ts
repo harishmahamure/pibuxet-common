d80c531ab02b5af4b1ac8f8ccf90ead35f195588
Harish <hmahamure@gmail.com>
2021-06-15 18:38:55 +0530

    Update

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+    red: number;
+    blue: number;
+    green: number;
+}
+
+const color: Color = {
+    red: 20,
+    blue: 10,
+    green: 12,
+};
+
+export default color;
+
+console.log(color);
